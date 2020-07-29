452d7e50b7d03af5b5bcc47d4e09370d20063d39
Denis <kirevden@gmail.com>
2020-07-29 10:08:29 +0300

    fix: 'fix error from console. fix rerender map but i need to fix rerender layers point'

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import CssBaseline from '@material-ui/core/CssBaseline'
 import Container from '@material-ui/core/Container'
 
@@ -8,13 +8,15 @@ import Content from './../pages/content/content';
 import Home from './../pages/home/home-container';
 import PageNotFound from './../pages/404/page-not-found';
 
+
+
 const App = () => {
 
     const renderSwitch = () => (
         <Switch>
-            <Route path="/content" component={Content}/>}
-            <Route exact path="/" component={Home}/>
-            <Route component={PageNotFound}/>
+            <Route path="/content" render={(props) => <Content {...props}/>} />
+            <Route exact path="/" render={(props) => <Home {...props}/>}/>
+            <Route render={(props) => <PageNotFound {...props}/>} />
         </Switch>
     );
 
@@ -31,7 +33,7 @@ const App = () => {
             </React.Fragment>
         </Router>
     )
-}
+};
 
 // Expose It
 export default App
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,94 +1,103 @@
 import ReactDOM from "react-dom";
 import PropTypes from 'prop-types';
-import React, {useRef, useEffect} from "react";
+import React, {useRef, useEffect, useState} from "react";
 import mapboxgl from "mapbox-gl";
-import labelLayerId, { popup } from './../../config'
+import labelLayerId, {popup} from './../../config'
 
 import Popup from "../../components/popup/popup";
 
-const Home = ({ features, layerOptions, setPointItem }) => {
+const Home = ({features, layerOptions, setPointItem}) => {
 
-	const mapContainerRef = useRef(null);
+	const [map, setMap] = useState(null);
+	const [feature, setFeature] = useState(features);
+	const mapContainer = useRef(null);
+	const mounted = useRef();
 	const popUpRef = useRef(new mapboxgl.Popup({offset: 15}));
 
 	useEffect(() => {
-		const map = new mapboxgl.Map({
-			container: mapContainerRef.current,
-			style: 'mapbox://styles/mapbox/light-v10',
-			center: [-74.00555714964867, 40.71331846247975],
-			zoom: 15.5,
-			pitch: 45,
-			bearing: -17.6,
-			antialias: true,
-		}, []);
 
-		map.on("load", () => {
-			map.addSource('pointsSource', {
-				type: "geojson",
-				data: {
-					type: 'FeatureCollection',
-					features: features,
-				},
+		const initializeMap = ({ setMap, mapContainer }) => {
+			let map = new mapboxgl.Map({
+				container: mapContainer.current,
+				style: 'mapbox://styles/mapbox/light-v10',
+				center: [-74.00555714964867, 40.71331846247975],
+				zoom: 15.5,
+				pitch: 45,
+				bearing: -17.6,
+				antialias: true,
+			}, []);
+
+			map.on("load", () => {
+				setMap(map);
+				map.addLayer(layerOptions, labelLayerId(map));
+				map.addSource('pointsSource', {
+					type: "geojson",
+					data: {
+						type: 'FeatureCollection',
+						features: features,
+					},
+				});
+
+				map.addLayer({
+					id: "points",
+					source: "pointsSource",
+					type: "circle",
+					paint: {
+						'circle-radius': 10,
+						'circle-color': 'skyblue'
+					}
+				});
+
 			});
-			map.addLayer({
-				id: "points",
-				source: "pointsSource",
-				type: "circle",
-				paint: {
-					'circle-radius': 10,
-					'circle-color': 'skyblue'
+
+			map.on('mouseenter', 'points', e => {
+				map.getCanvas().style.cursor = 'pointer';
+				const result = map.queryRenderedFeatures(e.point, {layers: ['points']});
+				let coordinates = result[0].geometry.coordinates;
+				let description = result[0].properties.description;
+				let title = result[0].properties.title;
+
+				let resInfoPopup = `<h1>${title}</h1><h3>${description}</h3>`;
+
+				while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+					coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
 				}
-			});
-			map.addLayer(layerOptions, labelLayerId(map));
-		});
-		map.on("moveend", async (e) => {
-			// fetch new data
-			const results = await features;
-			map.getSource('pointsSource').setData(results);
-		});
-		map.on('mouseenter', 'points', e => {
-			map.getCanvas().style.cursor = 'pointer';
-			const result = map.queryRenderedFeatures(e.point, {layers: ['points']})
-			let coordinates = result[0].geometry.coordinates;
-			let description = result[0].properties.description;
-			let title = result[0].properties.title;
-
-			let resInfoPopup = `<h1>${ title }</h1><h3>${description}</h3>`
-
-			while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-				coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-			}
-			popup
-				.setLngLat(coordinates)
-				.setHTML(resInfoPopup)
-				.addTo(map);
-		});
-		map.on('mouseleave', 'points', () => {
-			map.getCanvas().style.cursor = '';
-			popup.remove();
-		});
-		map.on('click', '3d-buildings', (e) => {
-			const resultBuilds = map.queryRenderedFeatures(e.point, {layers: ['3d-buildings']})
-
-			if (resultBuilds.length) {
-				const popupNode = document.createElement("div");
-				ReactDOM.render(<Popup addPoint={setPointItem} lngLat={e.lngLat}/>, popupNode);
-				popUpRef.current
-					.setLngLat(e.lngLat)
-					.setDOMContent(popupNode)
+				popup
+					.setLngLat(coordinates)
+					.setHTML(resInfoPopup)
 					.addTo(map);
-			}
-		});
-		// clean up on unmount
-		return () => map.remove();
-	}, [features, setPointItem, layerOptions]);
+			});
+			map.on('mouseleave', 'points', () => {
+				map.getCanvas().style.cursor = '';
+				popup.remove();
+			});
+			map.on('click', '3d-buildings', (e) => {
+				const resultBuilds = map.queryRenderedFeatures(e.point, {layers: ['3d-buildings']});
 
-	return <div className="map-container" ref={mapContainerRef}/>;
+				if (resultBuilds.length) {
+					const popupNode = document.createElement("div");
+					ReactDOM.render(<Popup addPoint={setPointItem} lngLat={e.lngLat}/>, popupNode);
+					popUpRef.current
+						.setLngLat(e.lngLat)
+						.setDOMContent(popupNode)
+						.addTo(map);
+				}
+				setFeature(features.length - 1)
+			});
+		};
+
+		if (!map) {
+			initializeMap({ setMap, mapContainer });
+		}
+	}, [map, features, layerOptions, setPointItem]);
+
+	return <div className="map-container" ref={mapContainer}/>;
 };
 
 Home.propTypes = {
 	features: PropTypes.array.isRequired,
 	layerOptions: PropTypes.object.isRequired,
+	setPointItem: PropTypes.func.isRequired,
 };
 
 export default Home;
