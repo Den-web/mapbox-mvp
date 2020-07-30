2880bd792adf25617fb6d2ede916fb2ad4100723
Oleh Raban (Zemelia) <o.raban@internetdevels.com>
2020-07-30 10:07:29 +0300

    Example of possible fix.

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -11,30 +11,24 @@ const Home = ({features, layerOptions, setPointItem}) => {
 	const mapContainer = useRef(null);
 	const popUpRef = useRef(new mapboxgl.Popup({offset: 15}));
 
+
 	useEffect(() => {
-        let mapInit = new mapboxgl.Map({
-            container: mapContainer.current,
-            style: 'mapbox://styles/mapbox/light-v10',
-            center: [-74.00555714964867, 40.71331846247975],
-            zoom: 15.5,
-            pitch: 45,
-            bearing: -17.6,
-            antialias: true,
-        }, []);
-
-
-        if(mapInit) {
-            setMap(mapInit);
-            console.log(mapInit)
-        }
+		if (!map) {
+      console.log('sss');
+      // We should proceed this inly once.
+      let mapObject = new mapboxgl.Map({
+        container: mapContainer.current,
+        style: 'mapbox://styles/mapbox/light-v10',
+        center: [-74.00555714964867, 40.71331846247975],
+        zoom: 15.5,
+        pitch: 45,
+        bearing: -17.6,
+        antialias: true,
     }, []);
-
-	useEffect(() => {
-		if (map) {
-            map.on("load", () => {
-                setMap(map);
-                map.addLayer(layerOptions, labelLayerId(map));
-                map.addSource('pointsSource', {
+    mapObject.on("load", () => {
+                setMap(mapObject);
+                mapObject.addLayer(layerOptions, labelLayerId(mapObject));
+                mapObject.addSource('pointsSource', {
                     type: "geojson",
                     data: {
                         type: 'FeatureCollection',
@@ -42,7 +36,7 @@ const Home = ({features, layerOptions, setPointItem}) => {
                     },
                 });
 
-                map.addLayer({
+                mapObject.addLayer({
                     id: "points",
                     source: "pointsSource",
                     type: "circle",
@@ -52,9 +46,9 @@ const Home = ({features, layerOptions, setPointItem}) => {
                     }
                 });
             });
-            map.on('mouseenter', 'points', e => {
-                map.getCanvas().style.cursor = 'pointer';
-                const result = map.queryRenderedFeatures(e.point, {layers: ['points']});
+            mapObject.on('mouseenter', 'points', e => {
+              mapObject.getCanvas().style.cursor = 'pointer';
+                const result = mapObject.queryRenderedFeatures(e.point, {layers: ['points']});
                 let coordinates = result[0].geometry.coordinates;
                 let description = result[0].properties.description;
                 let title = result[0].properties.title;
@@ -67,14 +61,14 @@ const Home = ({features, layerOptions, setPointItem}) => {
                 popup
                     .setLngLat(coordinates)
                     .setHTML(resInfoPopup)
-                    .addTo(map);
+                    .addTo(mapObject);
             });
-            map.on('mouseleave', 'points', () => {
-                map.getCanvas().style.cursor = '';
+            mapObject.on('mouseleave', 'points', () => {
+              mapObject.getCanvas().style.cursor = '';
                 popup.remove();
             });
-            map.on('click', '3d-buildings', (e) => {
-                const resultBuilds = map.queryRenderedFeatures(e.point, {layers: ['3d-buildings']});
+            mapObject.on('click', '3d-buildings', (e) => {
+                const resultBuilds = mapObject.queryRenderedFeatures(e.point, {layers: ['3d-buildings']});
 
                 if (resultBuilds.length) {
                     const popupNode = document.createElement("div");
@@ -82,20 +76,23 @@ const Home = ({features, layerOptions, setPointItem}) => {
                     popUpRef.current
                         .setLngLat(e.lngLat)
                         .setDOMContent(popupNode)
-                        .addTo(map);
+                        .addTo(mapObject);
                 }
             });
-
-            if (map.getSource('pointsSource') !== undefined) {
-                map.getSource('pointsSource').setData({
-                    type: 'FeatureCollection',
-                    features: features,
-                });
-                popUpRef.current.remove();
-            }
         }
 
-	}, [map, features, layerOptions, setPointItem]);
+  }, [map, features, layerOptions, setPointItem]);
+
+  useEffect(() => {
+    if (map && map.getSource('pointsSource')) {
+      map.getSource('pointsSource').setData({
+        type: 'FeatureCollection',
+        features: features,
+      });
+      popUpRef.current.remove();
+    }
+  }, [features, map]);
+
 
 	return <div className="map-container" ref={mapContainer}/>;
 };
